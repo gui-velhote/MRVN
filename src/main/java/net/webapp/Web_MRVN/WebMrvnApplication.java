988620b11df66901ce51872efae7b1956e6b0342
Guilherme Velhote <gui.velhote@gmail.com>
2022-12-12 11:53:45 -0300

    V2.0

---
+++ src/main/java/net/webapp/Web_MRVN/WebMrvnApplication.java
@@ -0,0 +1,23 @@
+package net.webapp.Web_MRVN;
+
+
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.ui.Model;
+
+
+
+
+
+@SpringBootApplication
+public class WebMrvnApplication {
+
+    public static void main(String[] args) {
+		SpringApplication.run(WebMrvnApplication.class, args);
+    }
+}
